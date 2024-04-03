e55ea4d274478c56ea61178396fa90e82841c4fd
SonYG1 <sonntph3878@fpt.edu.vn>
2024-04-03 23:09:36 +0700

    SonYG1

--- app/src/main/java/com/example/khachhang/Thanhtaon.java
+++ app/src/main/java/com/example/khachhang/Thanhtaon.java
@@ -57,7 +57,7 @@ public class Thanhtaon extends AppCompatActivity implements GioHangAdapter.OnIte
                 for (GioHang gioHang : danhSachSanPham) {
                     onItemClick(gioHang);
                 }
-                adapter.clear   
+                danhSachSanPham.clear();
 //                GioHang gioHang;
 //                if (getIntent().hasExtra("Gio Hang")) {
 //                    gioHang = getIntent().getParcelableExtra("Gio Hang");
